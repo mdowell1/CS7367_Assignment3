fb05bcdd8f175d9e0e0b3a44dc3665901be4fede
mdowell1 <99506206+mdowell1@users.noreply.github.com>
2023-02-25 17:20:41 -0500

    Add files via upload

--- Transformations.py
+++ Transformations.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 xHalf, yHalf = 0, 0
 
@@ -18,8 +19,8 @@ def rotateMatrix(x, y):
         angle = 0.1
 
     return np.array([[np.cos(angle), -np.sin(angle), 0],
-                  [np.sin(angle), np.cos(angle), 0],
-                  [0, 0, 1]])
+                     [np.sin(angle), np.cos(angle), 0],
+                     [0, 0, 1]])
 
 
 def scaleMatrix(x, y):
@@ -31,6 +32,38 @@ def scaleMatrix(x, y):
     return np.array([[scale, 0, 0],
                      [0, scale, 0],
                      [0, 0, 1]])
+
+
+def affineMatrix(coords):
+    newCoords = []
+    original = []
+
+    # affine only requires 3 points, so go to 6 instead of 8
+    for i in range(1, 6, 2):
+        pointRange = np.random.uniform(-5.0, 5.0, [1, 2])[0]
+        original.append([coords[i-1], coords[i]])
+        x = coords[i-1] + pointRange[0]
+        y = coords[i] + pointRange[1]
+        newCoords.append([x, y])
+
+    M = cv2.getAffineTransform(np.float32(original), np.float32(newCoords))
+    return np.vstack([M, [1, 1, 1]])  # adds row of 1s for math
+
+
+def projectiveMatrix(coords):
+    newCoords = []
+    original = []
+
+    # affine only requires 3 points, so go to 6 instead of 8
+    for i in range(1, 8, 2):
+        pointRange = np.random.uniform(-2.0, 2.0, [1, 2])[0]
+        original.append([coords[i - 1], coords[i]])
+        x = coords[i - 1] + pointRange[0]
+        y = coords[i] + pointRange[1]
+
+        newCoords.append([x, y])
+
+    return cv2.getPerspectiveTransform(np.float32(original), np.float32(newCoords))
 
 
 # endregion
@@ -113,6 +146,30 @@ def similarity(coords, lastPoint, newLoc):
     M = originTranslation @ MS @ MR @ MT @ returnTranslation @ coordArray
 
     # get the new x, y coordinates as a list
+    return matrixToCoords(M)
+
+
+def affine(coords):
+    # convert coordinates to a numpy array and reshape them for matrix math
+    coordArray = coordsToMatrix(coords)
+    MA = affineMatrix(coords)  # get the affine matrix
+
+    # do affine transform
+    M = MA @ coordArray
+    return matrixToCoords(M)
+
+
+def projective(coords):
+    # convert coordinates to a numpy array and reshape them for matrix math
+    coordArray = coordsToMatrix(coords)
+    originTranslation = translateMatrix(xHalf, yHalf)  # to move translation origin to center of canvas
+    returnTranslation = translateMatrix(-xHalf, -yHalf)  # to move translation origin back to top left
+
+    MA = projectiveMatrix(coords)
+
+    # first, move the coordinates to the new origin, then do affine transform, then
+    # return to the original origin, then apply changes to the original coordinates
+    M = originTranslation @ MA @ returnTranslation @ coordArray
     return matrixToCoords(M)
 
 # endregion
